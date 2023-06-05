f094ace27c1190229583ba154311e109a133e985
vine96 <viniciuspecci@hotmail.com>
2023-06-05 07:51:37 -0300

    Modify file with node using module fs

--- modules/fs.js
+++ modules/fs.js
@@ -1,13 +1,14 @@
+const { error } = require('console');
 const fs = require('fs')
 const path = require('path')
 
 // Criar uma pasta
-fs.mkdir(path.join(__dirname, '/test'), (error) => {
-    if(error){
-        return console.log("Ocorreu o erro: ", error);
-    }
-    console.log("Pasta criada com sucesso!");
-})
+// fs.mkdir(path.join(__dirname, '/test'), (error) => {
+//     if(error){
+//         return console.log("Ocorreu o erro: ", error);
+//     }
+//     console.log("Pasta criada com sucesso!");
+// })
 
 // Criar um arquivo
 fs.writeFile(path.join(__dirname, '/test', 'test.txt'), 'hello node!', (error) => {
@@ -15,4 +16,12 @@ fs.writeFile(path.join(__dirname, '/test', 'test.txt'), 'hello node!', (error) =
         return console.log("Ocorreu o erro: ", error);
     }
     console.log("Arquivo criado com sucesso!");
+})
+
+// Adicionar conteúdo a um arquivo
+fs.appendFile(path.join(__dirname, '/test', 'test.txt'), ' hello word!', (error) => {
+    if(error){
+        return console.log("Erro encontrado: ", error);
+    }
+    console.log("Conteúdo adicionado com sucesso ao arquivo!");
 })

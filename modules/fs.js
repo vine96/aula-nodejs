1b4ee625576f42862d2b748c0b3001642210d150
vine96 <viniciuspecci@hotmail.com>
2023-06-05 07:46:47 -0300

    Make file with node using module fs

--- modules/fs.js
+++ modules/fs.js
@@ -8,3 +8,11 @@ fs.mkdir(path.join(__dirname, '/test'), (error) => {
     }
     console.log("Pasta criada com sucesso!");
 })
+
+// Criar um arquivo
+fs.writeFile(path.join(__dirname, '/test', 'test.txt'), 'hello node!', (error) => {
+    if(error){
+        return console.log("Ocorreu o erro: ", error);
+    }
+    console.log("Arquivo criado com sucesso!");
+})

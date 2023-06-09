d827680ecd9aab94fd6977291bc6bb5a6c52a794
vine96 <viniciuspecci@hotmail.com>
2023-06-09 07:21:28 -0300

    route findUserById

--- modules/express.js
+++ modules/express.js
@@ -13,6 +13,16 @@ app.get('/users', async (req, res) => {
     }
 });
 
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
 app.post('/users', async (req, res) => {
     try {
         const user = await UserModel.create(req.body);

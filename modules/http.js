2da33e6f32d2685f1be8674b123125b29b145178
vine96 <viniciuspecci@hotmail.com>
2023-06-08 22:31:49 -0300

    Simple pages using nodeJS

--- modules/http.js
+++ modules/http.js
@@ -3,6 +3,25 @@ const http = require('http')
 const port = 8080;
 const server = http.createServer((req, res) => {
     if(req.url == "/home"){
-        res.end("<h1>Home page</h1>")
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end("<h1>Home page</h1>");
+    }
+
+    if (req.url == "/users") {
+        const users = [
+            {
+                name: 'Alfredo Menezes',
+                email: 'amtrosul@hotmail.com'
+            },
+            {
+                name: 'Jurema Bittencurt',
+                email: 'juremarosario@hotmail.com'
+            }
+        ];
+    
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(users));
     }
 });
+
+server.listen(port, () => console.log(`Rodando na porta ${port}!`));

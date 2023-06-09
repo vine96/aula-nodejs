03666aba1646d0ef385431562b910f596237815d
vine96 <viniciuspecci@hotmail.com>
2023-06-09 07:08:14 -0300

    Create users route

--- modules/express.js
+++ modules/express.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const UserModel = require('../src/models/user.model')
 
 const app = express();
+app.use(express.json());
 
 app.get('/home', (req, res) => {
     res.contentType('.html');
@@ -19,6 +21,16 @@ app.get('/users', (req, res) => {
         }
     ];
     res.status(200).json(users);
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 const port = 8080;

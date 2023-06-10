b9cf7387db39d8d098e1d339bb092089e0638e9f
vine96 <viniciuspecci@hotmail.com>
2023-06-10 07:57:25 -0300

    Using middleware

--- modules/express.js
+++ modules/express.js
@@ -4,6 +4,13 @@ const UserModel = require('../src/models/user.model')
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`Request Type: ${req.method}`);
+    console.log(`Content Type: ${req.headers["content-type"]}`);
+    console.log(`Date: ${new Date()}`);
+    next();
+});
+
 // Find all users
 app.get('/users', async (req, res) => {
     try {

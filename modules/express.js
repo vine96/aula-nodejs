820e452af0baf7b4ec687b6ffcd55955c34cb05c
vine96 <viniciuspecci@hotmail.com>
2023-06-10 08:12:10 -0300

    Construct a simple page html using EJS

--- modules/express.js
+++ modules/express.js
@@ -4,11 +4,18 @@ const UserModel = require('../src/models/user.model')
 const app = express();
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
 app.use((req, res, next) => {
     console.log(`Request Type: ${req.method}`);
     console.log(`Content Type: ${req.headers["content-type"]}`);
     console.log(`Date: ${new Date()}`);
     next();
+});
+
+app.get('/views/users', async (req, res) => {
+    res.render("index");
 });
 
 // Find all users
